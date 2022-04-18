1b9d3de92b4889f73bf1de6bde7273456474dcb0
alexdai186 <alexdai186@gmail.com>
2022-04-18 18:32:59 -0500

    line with kanav

--- src/randomtaker.ts
+++ src/randomtaker.ts
@@ -64,7 +64,7 @@ const params = JSON.parse(
 const payer = new Account(
   JSON.parse(
     fs.readFileSync(
-      process.env.KEYPAIR || os.homedir() + '/.config/solana/id.json',
+      process.env.KEYPAIR || os.homedir() + '/.config/solana/entropy-taker.json',
       'utf-8',
     ),
   ),
